9aef2b918a56a40eea4c7e7573e0a416aeef9e79
Gamy Malasarte <gmaasarte@Gamys-Mac.local>
2017-06-15 22:00:32 -0700

    Cleaned up

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -46,101 +46,52 @@ class ViewController: UIViewController {
                 user.username = usernameTextField.text
                 user.password = passwordTextField.text
                 
-                
                 user["isDriver"] = userSignupSwitch.isOn
-                
                 user.signUpInBackground(block: { (success, error) in
                     
                     if let error = error {
-                        
                         var displayedErrorMessage = "Please try again later"
-                        
                         let error = error as NSError
-                        
                         if let parseError = error.userInfo["error"] as? String {
-                            
                             displayedErrorMessage = parseError
-                            
-                            
                         }
-                        
                         self.displayAlert(title: "Sign Up Failed", message: displayedErrorMessage)
-                        
                     } else {
                         
                         print("Sign Up Successful")
-                        
                         if let isDriver = PFUser.current()?["isDriver"] as? Bool {
-                            
                             if isDriver {
-                                
                                 self.performSegue(withIdentifier: "showDriverViewController", sender: self)
-                                
-                                
                             } else {
-                                
-                                
                                 self.performSegue(withIdentifier: "showRiderViewController", sender: self)
-                                
                             }
-                            
-                            
-                            
                         }
-                        
                     }
-                    
-                    
                 })
                 
             } else {
                 
                 PFUser.logInWithUsername(inBackground: usernameTextField.text!, password: passwordTextField.text!, block: { (user, error) in
-                    
                     if let error = error {
-                        
                         var displayedErrorMessage = "Please try again later"
-                        
                         let error = error as NSError
-                        
                         if let parseError = error.userInfo["error"] as? String {
-                            
                             displayedErrorMessage = parseError
-                            
-                            
                         }
-                        
                         self.displayAlert(title: "Sign Up Failed", message: displayedErrorMessage)
-                        
                     } else {
                         
                         print("Log In Successful")
-                        
                         if let isDriver = PFUser.current()?["isDriver"] as? Bool {
-                            
                             if isDriver {
-                                
                                 self.performSegue(withIdentifier: "showDriverViewController", sender: self)
-                                
                             } else {
-                                
-                                
                                 self.performSegue(withIdentifier: "showRiderViewController", sender: self)
-                                
                             }
-                            
-                            
-                            
                         }
-
-                        
                     }
-                    
-                    
                 })
-                
             }
-            
         }
         
 
@@ -154,32 +105,21 @@ class ViewController: UIViewController {
         
         
         if signUpMode {
-            
             signupOrLoginButton.setTitle("Log In", for: [])
-            
             signupSwitchButton.setTitle("Switch To Sign Up", for: [])
-            
             signUpMode = false
-            
             userSignupSwitch.isHidden = true
-            
             riderLabel.isHidden = true
-            
             driverLabel.isHidden = true
             
             
         } else {
             
             signupOrLoginButton.setTitle("Sign Up", for: [])
-            
             signupSwitchButton.setTitle("Switch To Log In", for: [])
-            
             signUpMode = true
-            
             userSignupSwitch.isHidden = false
-            
             riderLabel.isHidden = false
-            
             driverLabel.isHidden = false
             
         }
@@ -189,24 +129,14 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         
         if let isDriver = PFUser.current()?["isDriver"] as? Bool {
-            
             if isDriver {
-                
                 performSegue(withIdentifier: "showDriverViewController", sender: self)
-                
-                
             } else {
-                
-                
                 self.performSegue(withIdentifier: "showRiderViewController", sender: self)
-                
             }
             
-            
-            
         }
-
-        
+ 
         
     }
     
